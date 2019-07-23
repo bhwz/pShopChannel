2d779818e0b6fa99374e862d3fa291c8b4bc5dc2
Benjamin H <benjamin.hallowitz@gmail.com>
2019-07-23 11:56:40 -0700

    Implement a paginated catalog.
    As a side effect, searching and browsing are now more tightly coupled.

--- app/views/catalog.py
+++ app/views/catalog.py
@@ -1,22 +1,24 @@
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, url_for
+
+from app.models import Product
 
 blueprint = Blueprint('catalog', __name__, url_prefix='/catalog')
 
 
-@blueprint.route('/browse', methods=['GET'])
-def catalog():
-    return render_template('storefront/catalog.html')
+@blueprint.route('', methods=['GET'])
+def browse():
+    search = request.args.get('search', None, type=str)
+    page = request.args.get('page', 1, type=int)
+    max_results = 8
+    # TODO: More filtering based on search.
+    products = Product.query.filter_by(parent_id=None, published=True).paginate(page, max_results, False)
+    next_url = url_for('catalog.browse', page=products.next_num) if products.has_next else None
+    prev_url = url_for('catalog.browse', page=products.prev_num) if products.has_prev else None
+    title = 'Search: ' + search if search else 'Catalog'
 
-
-@blueprint.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    if not query:
-        return render_template('storefront/search.html', query=query)
-
-    # TODO: Do Search
-    result = 'Some results.'
-    return render_template('storefront/search.html', query=query, result=result)
+    return render_template(
+        'storefront/catalog.html', title=title, search=search, products=products.items, next_url=next_url, prev_url=prev_url
+    )
 
 
 @blueprint.route('/product/<int:pid>', methods=['GET'])
