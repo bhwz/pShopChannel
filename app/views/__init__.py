9a8f385f40b39080a414d0df813bc49931776189
Benjamin H <benjamin.hallowitz@gmail.com>
2019-08-14 10:20:09 -0700

    Temporarily remove backoffice.
    This will help with the upcoming changes to allow easy swapping of custom themes.

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,11 +1,9 @@
 from app.views.pages import blueprint as pages_blueprint
 from app.views.catalog import blueprint as catalog_blueprint
 from app.views.account import blueprint as account_blueprint
-from app.views.backoffice import blueprint as backoffice_blueprint
 
 
 def register_blueprints(app):
     app.register_blueprint(pages_blueprint)
     app.register_blueprint(catalog_blueprint)
     app.register_blueprint(account_blueprint)
-    app.register_blueprint(backoffice_blueprint)
--- app/views/backoffice.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-blueprint = Blueprint('backoffice', __name__, url_prefix='/backoffice')
-
-
-@blueprint.route('dash', methods=['GET'])
-def dashboard():
-    return render_template('backoffice/dashboard.html', title='Dashboard')
